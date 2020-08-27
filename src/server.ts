b67df5c8965de5e5e61dcb8594df3445ee6bd760
Reginaldo Boeke <reginaldoboeke@MacBook-Pro-de-Reginaldo.local>
2020-08-27 20:21:30 -0300

    Commit inicial

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
-import routes from './routes';
 
+import routes from './routes';
 import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
 
@@ -11,6 +12,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
